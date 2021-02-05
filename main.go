49c9ab41605acc5289f5f6ebb411508bbbdcbd11
lemon-mint <55233766+lemon-mint@users.noreply.github.com>
2021-02-05 14:05:53 +0900

    wipe21

--- main.go
+++ main.go
@@ -13,3 +13,51 @@ func main() {
 		}
 	}
 }
+
+func wipe21(filename string) error {
+	err := wiper.RandFill(filename, 10)
+	if err != nil {
+		return err
+	}
+	err = wiper.ZeroFill(filename, 2)
+	if err != nil {
+		return err
+	}
+	err = wiper.ComplementFill(filename)
+	if err != nil {
+		return err
+	}
+	err = wiper.RandFill(filename, 3)
+	if err != nil {
+		return err
+	}
+	err = wiper.ComplementFill(filename)
+	if err != nil {
+		return err
+	}
+	err = wiper.RandFill(filename, 3)
+	if err != nil {
+		return err
+	}
+	filename, err = wiper.MixFileName(filename, 10)
+	if err != nil {
+		return err
+	}
+	err = wiper.MixTime(filename, 10)
+	if err != nil {
+		return err
+	}
+	filename, err = wiper.MixFileName(filename, 5)
+	if err != nil {
+		return err
+	}
+	err = wiper.MixTime(filename, 5)
+	if err != nil {
+		return err
+	}
+	err = wiper.ZeroFill(filename, 1)
+	if err != nil {
+		return err
+	}
+	return os.Remove(filename)
+}
